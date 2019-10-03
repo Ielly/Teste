bb4b93171e007eeb97e67b8531977e2a308fb4e6
Ielly <gabrielly.tenafonseca@gmail.com>
2019-10-03 12:33:12 -0300

    Commit Exercicio

---
+++ Documents/NetBeansProjects/Escola/src/escola/Aluno.java
@@ -0,0 +1,72 @@
+
+package escola;
+
+
+public class Aluno {
+    
+    private String Nome;
+    private int Cod;
+    private int nota1;
+    private int nota2;
+    private int nota3;
+    private int nota4;
+
+   
+     public String getNome(){
+    return Nome;
+   }
+    public void setNome(String nome){
+    this.Nome = nome;
+    }
+    public int getCod(){
+    return Cod;
+    }
+    
+    public void setCod(int cod){
+        this.Cod = cod;
+        
+    }
+     public int getNota1(){
+    return nota1;
+   }
+    public void setNota1(int nota){
+    this.nota1 = nota;
+    }
+    public int getNota2(){
+    return nota1;
+   }
+    public void setNota2(int nota){
+    this.nota2 = nota;
+    }
+    public int getNota3(){
+    return nota1;
+   }
+    public void setNota3(int nota){
+    this.nota3 = nota;
+    }
+    public int getNota4(){
+    return nota4;
+   }
+    public void setNota4(int nota){
+    this.nota4 = nota;
+    }
+    
+    public void exibirNome(){
+    
+    }
+    public void exibirCod(){
+    
+}
+    public void exibirNota1(){
+}
+    public void exibirNota2(){
+    }
+
+    public void exibirNota3(){
+    }
+    
+        public void exibirNota4(){
+    
+        }
+}
+
---
+++ Documents/NetBeansProjects/Escola/src/escola/Principal.java
@@ -0,0 +1,95 @@
+
+package escola;
+
+
+public class Principal {
+
+  
+    public static void main(String[] args) {
+        
+        
+     Aluno Rodolfo = new Aluno();
+        Rodolfo.setNome("Aluno: ");
+        
+   Rodolfo.setCod(0643);
+   
+    
+    Rodolfo.setNota1(0);
+    
+     Rodolfo.setNota2(0);
+  
+     Rodolfo.setNota3(0);
+   
+     Rodolfo.setNota4(0);
+   
+        
+    
+    
+        Aluno Alfredo = new Aluno();
+        Alfredo.setNome("Aluno: ");
+        
+        Alfredo.setCod(0453);
+    
+        Alfredo.setNota1(7);
+    
+     Alfredo.setNota2(4);
+  
+     Alfredo.setNota3(0);
+   
+     Alfredo.setNota4(5);
+        
+        
+    
+    
+        Aluno Jonas = new Aluno();
+        Jonas.setNome("Aluno: ");
+        
+        Jonas.setCod(053);
+    
+        Jonas.setNota1(0);
+    
+     Jonas.setNota2(3);
+  
+     Jonas.setNota3(7);
+   
+     Jonas.setNota4(1);
+        
+        
+    
+    
+        Aluno Juliana = new Aluno();
+        Juliana.setNome("Aluno: ");
+        
+        Juliana.setCod(0353);
+        
+        Juliana.setNota1(10);
+    
+     Juliana.setNota2(9);
+  
+     Juliana.setNota3(9);
+   
+     Juliana.setNota4(8);
+    
+    
+        Aluno Bartolomeia = new Aluno();
+        Bartolomeia.setNome("Aluno: ");
+        
+        Bartolomeia.setCod(06433);
+        
+        Bartolomeia.setNota1(10);
+    
+     Bartolomeia.setNota2(9);
+  
+     Bartolomeia.setNota3(10);
+   
+     Bartolomeia.setNota4(10);
+     
+        System.out.println("Nome:"+Rodolfo);
+        if(Rodolfo.getNota1+Rodolfo.getNota2+RodolfogetNota3+Nota4==<5){
+        System.out.println("Você foi reprovado");
+        
+        else
+                System.out.println("Você passou de ano sz");
+                }
+    }
+}
